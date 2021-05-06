adf6b3f04e42dd0c3e5dc7af9b32ce57ea8aa037
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-05-06 11:11:50 +0400

    Style(EventCard):box shadow

--- components/buttons/Button.vue
+++ components/buttons/Button.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button
-      class="button focus:outline-none uppercase py-2 px-4 self-center text-white border-gray-700 text-xs"
+      class="button focus:outline-none uppercase py-2 px-4 self-center text-white text-xs"
       :class="type"
     >
       <slot></slot>
@@ -38,5 +38,9 @@ export default {
 .button.red-gradient {
   background: linear-gradient(45deg, rgb(157, 2, 8, 0.6), rgb(208, 0, 0, 0.9))
     100%;
+}
+.button.white {
+  color: black;
+  background-color: transparent;
 }
 </style>
--- components/game/EventCard.vue
+++ components/game/EventCard.vue
@@ -5,7 +5,12 @@
       <li id="event__past" class="box__title--label">Passé</li>
     </ul>
     <div class="box__subtitle--label">7 Tournois</div>
-    <div class="box__content">
+
+    <div
+      v-for="slot in event.phase.mainCard.slots"
+      :key="slot.id"
+      class="box__content"
+    >
       <div class="box__event--logo">
         <img
           :src="require('@/assets/images/game-logo/Mortal-Kombat-Embleme.png')"
@@ -19,11 +24,12 @@
               <button class="slick__prev"></button>
               <div class="slick__list">
                 <img
-                  :src="require('@/assets/images/users/avatar_1.png')"
+                  :src="require('@/assets/images/users/avatar-1.png')"
                   alt=""
                 />
+                <div class="slick__list--versus-img">Vs.</div>
                 <img
-                  :src="require('@/assets/images/users/avatar_2.png')"
+                  :src="require('@/assets/images/users/avatar-2.png')"
                   alt=""
                 />
               </div>
@@ -33,12 +39,17 @@
         </div>
       </div>
       <div class="box__event--infos">
-        <h3>Mr5000 VS ValveOverKil</h3>
+        <h3>
+          {{ slot.participant1.name }}
+          <span class="box__event--infos-versus">Vs.</span>
+          {{ slot.participant2.name }}
+        </h3>
         <span>Sun, May 9 / 4:00 AM +04 / Main Card</span>
+        <p>Online</p>
+        <p>Format: FT7</p>
       </div>
       <div class="box__event--link">
-        <Button type="red-gradient"> Regarder</Button>
-        <Button> Regarder</Button>
+        <Button class="btn" type="white"> Regarder</Button>
       </div>
     </div>
     <div></div>
@@ -50,6 +61,51 @@ export default {
   name: 'EventCard',
   components: {
     Button,
+  },
+  data() {
+    return {
+      text: 'hello',
+      event: {
+        phase: {
+          mainCard: {
+            slots: {
+              id1234: {
+                participant1: {
+                  name: 'Mr.5000',
+                },
+                participant2: {
+                  name: 'ValveOverKill',
+                },
+              },
+              id12345: {
+                participant1: {
+                  name: 'MarkTheShark',
+                },
+                participant2: {
+                  name: 'KillJoy',
+                },
+              },
+              id123456: {
+                participant1: {
+                  name: 'Epok',
+                },
+                participant2: {
+                  name: 'ParaSurama',
+                },
+              },
+              id1234567: {
+                participant1: {
+                  name: 'Atrocy ZA',
+                },
+                participant2: {
+                  name: 'Bliono',
+                },
+              },
+            },
+          },
+        },
+      },
+    }
   },
 }
 </script>
@@ -84,18 +140,25 @@ export default {
     }
   }
   &__content {
-    width: 85%;
+    width: 100%;
     height: auto;
     display: flex;
     justify-content: space-around;
     align-items: center;
-    margin-top: 5rem;
+    margin: 1rem 0 1rem 0;
+    padding: 1rem 0;
+    transition: 0.5s ease-in-out;
 
     @media (max-width: 768px) {
       flex-direction: column;
       align-items: center;
       margin-top: 2rem;
     }
+  }
+  &__content:hover {
+    -webkit-box-shadow: 2px -1px 52px 11px rgba(201, 201, 201, 1);
+    -moz-box-shadow: 2px -1px 52px 11px rgba(201, 201, 201, 1);
+    box-shadow: 2px -1px 52px 11px rgba(201, 201, 201, 1);
   }
   &__event {
     &--logo {
@@ -111,6 +174,31 @@ export default {
     }
     &--infos {
       color: black;
+      h3 {
+        text-transform: uppercase;
+        font-size: 1.5rem;
+      }
+      p {
+        font-size: 0.9rem;
+        font-family: 'Open-Sans' 'Condensed';
+        font-weight: 400;
+        color: #585b63;
+        text-transform: capitalize;
+      }
+      &-versus {
+        color: #9999a9;
+        text-transform: capitalize;
+      }
+    }
+    &--link {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      > .btn {
+        border: 1px solid #9999a9;
+      }
     }
   }
 }
@@ -123,5 +211,21 @@ export default {
 #event__past {
   color: #9999a9;
   cursor: pointer;
+}
+.slick__list {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 230px;
+    height: 160px;
+    object-fit: contain;
+  }
+  &--versus-img {
+    font-family: 'Roboto';
+    font-weight: 700;
+    font-size: 1.5rem;
+  }
 }
 </style>
