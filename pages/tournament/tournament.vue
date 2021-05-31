0dae5dd5b83cab9ed933763cb1ea452196918b12
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-05-31 17:40:12 +0400

    Feature(bracket): Style

---
+++ pages/tournament/tournament.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <tournamentBracket></tournamentBracket>
+    <div>hello tournament</div>
+  </div>
+</template>
+
+<script>
+import tournamentBracket from '@/components/tournament/Bracket'
+export default {
+  components: {
+    tournamentBracket,
+  },
+}
+</script>
+
+<style></style>
