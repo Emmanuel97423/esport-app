c692a40a4bc355ef3dfea2cb747a70ef3e507beb
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-06-01 15:35:45 +0400

    Feature(bracket-viewer): test en plug in

--- components/tournament/Bracket.vue
+++ components/tournament/Bracket.vue
@@ -6,32 +6,11 @@
 </template>
 
 <script>
-import { bracketsViewer } from '~/api/brackets-viewer.min.js'
 export default {
   data() {
     return { data: '', isBracketLoaded: true }
   },
 
-  head() {
-    return {
-      title: 'bracket',
-      script: [
-        {
-          hid: 'bracketsViewer',
-          src:
-            'https://cdn.jsdelivr.net/gh/Drarig29/brackets-viewer.js@master/dist/brackets-viewer.min.js',
-          body: true,
-        },
-      ],
-      link: [
-        {
-          rel: 'stylesheet',
-          href:
-            'https://cdn.jsdelivr.net/npm/brackets-viewer/dist/brackets-viewer.min.css',
-        },
-      ],
-    }
-  },
   beforeCreated() {},
   mounted() {
     this.bracketsViewerMethod()
