19b3b34ca87a180e71b0a77292e514fd0d515687
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-06-08 15:47:57 +0400

    Fix

--- components/tournament/Bracket.vue
+++ components/tournament/Bracket.vue
@@ -1,14 +1,43 @@
 <template>
-  <div></div>
+  <div>
+    <div v-if="isBracketLoaded" id="exemple" class="brackets-viewer"></div>
+    <button @click="bracketsViewerMethod">DATA</button>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {},
+  data() {
+    return { data: '', isBracketLoaded: true }
+  },
 
   beforeCreated() {},
-  mounted() {},
-  methods: {},
+  mounted() {
+    this.bracketsViewerMethod()
+  },
+  methods: {
+    async bracketsViewerMethod() {
+      const data = await this.$axios.$get('http://localhost:8000/api/bracket')
+      this.data = data
+
+      bracketsViewer.render(
+        {
+          stages: data.stage,
+          matches: data.match,
+          matchGames: data.match_game,
+          participants: data.participant,
+        },
+        {
+          selector: '#example',
+          participantOriginPlacement: 'before',
+          separatedChildCountLabel: true,
+          showSlotsOrigin: true,
+          showLowerBracketSlotsOrigin: true,
+          highlightParticipantOnHover: true,
+        }
+      )
+    },
+  },
 }
 </script>
 
