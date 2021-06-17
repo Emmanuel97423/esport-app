3be65e9c77183db83eaaf0a26321fe7ad5e7390c
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-06-17 10:27:05 +0400

    Feature(admin-section):auth

---
+++ pages/admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Admin section
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+import Cookie from 'js-cookie'
+export default {
+  asyncData({ $config, redirect }) {
+    if (!Cookie.get($config.auth.cookieName)) {
+      redirect('/no-access/')
+    }
+  },
+}
+</script>
+
+<style></style>
